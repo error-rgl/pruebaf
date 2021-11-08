ff36a41f9b072713db277dd2bfa01e1311b8d561
pether20 <pether.te.z@hotmail.com>
2021-11-08 01:24:45 +0000

    changes en line 4

--- prin.c
+++ prin.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-int main()
-{
-	int vars = 5;
-	float var1, var2; /*declaracion de variale tipo real*/
-
-	printf("este mensaje se imprime");/*instruccion de escritura*/
+printf("este mensaje se imprime");/*instruccion de escritura*/
 	return(0);
 }
